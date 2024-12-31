a880eb9b6c5a3ef95335f413053f756e85e08360
Peter Knut <peter@pematon.com>
2024-12-31 01:16:21 +0100

    Add several modes of main navigation
    - Add navigationMode and preferSelection config options.
    - Use <nav> and <menu> tags for navigation.

--- adminer/core/AdminerBase.php
+++ adminer/core/AdminerBase.php
@@ -207,7 +207,12 @@ abstract class AdminerBase
 
 	public abstract function databasesPrint($missing);
 
-	public abstract function printTablesFilter();
+	public function printTablesFilter() {
+		echo "<div class='tables-filter jsonly'>"
+			. "<input id='tables-filter' type='search' class='input' autocomplete='off' placeholder='" . lang('Table') . "'>"
+			. script("initTablesFilter(" . json_encode($this->database()) . ");")
+			. "</div>\n";
+	}
 
 	public abstract function tablesPrint(array $tables);
 
